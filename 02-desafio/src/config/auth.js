b375f97b3c6c41226ed3518cf48d03ee60b28dcd
glauberc <glaubercarv@gmail.com>
2019-08-21 17:21:11 -0300

    desafio02: sessao criada

---
+++ 02-desafio/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3287ca7a180d653c368795c4b6820cab',
+  expiresIn: '7d',
+};
--- 02-desafio/src/index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const server = express();
-
-server.get('/', (req, res) => res.send({ message: 'olá desafio' }));
-
-server.listen(3333);
