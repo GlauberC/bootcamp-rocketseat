3de566b2f2aebbf9e272ca3a599e43fad1d7bfa4
glauberc <glaubercarv@gmail.com>
2019-08-27 16:40:25 -0300

    02: added provider

--- 02-desafio/src/app/controllers/UserController.js
+++ 02-desafio/src/app/controllers/UserController.js
@@ -21,11 +21,12 @@ class UserController {
     if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
     }
-    const { id, name, email } = await User.create(req.body);
+    const { id, name, email, provider } = await User.create(req.body);
     return res.json({
       id,
       name,
       email,
+      provider,
     });
   }
 
@@ -60,9 +61,9 @@ class UserController {
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
-    const { id, name } = await user.update(req.body);
+    const { id, name, provider } = await user.update(req.body);
 
-    return res.json({ id, name, email });
+    return res.json({ id, name, email, provider });
   }
 }
 
