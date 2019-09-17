4fc2d9fb012f8f0b21a34fb6aba6f4dbfc4b858e
glauberc <glaubercarv@gmail.com>
2019-09-17 17:14:04 -0300

    07: styles

--- 07-flux-redux/src/App.js
+++ 07-flux-redux/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
+import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Alo</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <GlobalStyle />
+      <Routes />
+    </BrowserRouter>
   );
 }
 
